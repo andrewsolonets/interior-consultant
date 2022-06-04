4e706d4603cfd2466e2117c89022b91eebe607a4
Andrew Solonets <andrewsolonets@gmail.com>
2022-06-04 22:02:29 +0100

    Added scrolling animation on text

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
-const hamburger = document.querySelector (".hamburger");
-const block_menu_m = document.querySelector (".block_menu_m");
+const hamburger = document.querySelector(".hamburger");
+const block_menu_m = document.querySelector(".block_menu_m");
+
+const allSections = document.querySelectorAll(".section");
 
 hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    block_menu_m.classList.toggle("active");
-})
+  hamburger.classList.toggle("active");
+  block_menu_m.classList.toggle("active");
+});
 
-document.querySelectorAll(".block_m_2").forEach(n => n.
-  addEventListener("click", () => {
+document.querySelectorAll(".block_m_2").forEach((n) =>
+  n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     block_menu_m.classList.remove("active");
-  }))
+  })
+);
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  // console.log(entry);
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+};
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.12,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
